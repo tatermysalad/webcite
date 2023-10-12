6492b39b3cb08337abb9728481cea1f70af36ae1
tatermysalad <rosette.rural0c@icloud.com>
2023-10-12 18:53:32 +0200

    feat: added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import ApiProvider from "./contexts/ApiContext.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage.jsx";
 import BackgroundComponent from "./components/BackgroundComponent.jsx";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
